101b8cacbe4d16f1c220f2f928150c5cdaacef3c
FacePunch1337 <rezol1337@gmail.com>
2022-04-14 09:40:30 +0200

    +

--- GDI/Forms/GDIForm.Designer.cs
+++ GDI/Forms/GDIForm.Designer.cs
@@ -30,32 +30,193 @@ namespace GDI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelBallCoord = new System.Windows.Forms.Label();
+            this.labelMouseCoord = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelMissed = new System.Windows.Forms.Label();
+            this.labelGameOver = new System.Windows.Forms.Label();
+            this.pictureRestart = new System.Windows.Forms.PictureBox();
+            this.pictureQuit = new System.Windows.Forms.PictureBox();
+            this.picturePause = new System.Windows.Forms.PictureBox();
+            this.panelDisplay = new System.Windows.Forms.Panel();
+            this.timerBallSpawn = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRestart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureQuit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePause)).BeginInit();
+            this.panelDisplay.SuspendLayout();
             this.SuspendLayout();
             // 
-            // timer1
+            // timer
             // 
-            this.timer1.Interval = 20;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labelBallCoord
+            // 
+            this.labelBallCoord.AutoSize = true;
+            this.labelBallCoord.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBallCoord.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelBallCoord.Location = new System.Drawing.Point(203, 294);
+            this.labelBallCoord.Name = "labelBallCoord";
+            this.labelBallCoord.Size = new System.Drawing.Size(25, 13);
+            this.labelBallCoord.TabIndex = 0;
+            this.labelBallCoord.Text = "0.0";
+            // 
+            // labelMouseCoord
+            // 
+            this.labelMouseCoord.AutoSize = true;
+            this.labelMouseCoord.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMouseCoord.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelMouseCoord.Location = new System.Drawing.Point(203, 307);
+            this.labelMouseCoord.Name = "labelMouseCoord";
+            this.labelMouseCoord.Size = new System.Drawing.Size(25, 13);
+            this.labelMouseCoord.TabIndex = 1;
+            this.labelMouseCoord.Text = "0.0";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.labelTimer.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimer.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelTimer.Location = new System.Drawing.Point(3, 10);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(55, 13);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "00:00:00";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScore.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelScore.Location = new System.Drawing.Point(64, 10);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(55, 13);
+            this.labelScore.TabIndex = 3;
+            this.labelScore.Text = "Score: 0";
+            // 
+            // labelMissed
+            // 
+            this.labelMissed.AutoSize = true;
+            this.labelMissed.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMissed.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelMissed.Location = new System.Drawing.Point(125, 10);
+            this.labelMissed.Name = "labelMissed";
+            this.labelMissed.Size = new System.Drawing.Size(61, 13);
+            this.labelMissed.TabIndex = 4;
+            this.labelMissed.Text = "Missed: 0";
+            // 
+            // labelGameOver
+            // 
+            this.labelGameOver.AutoSize = true;
+            this.labelGameOver.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelGameOver.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGameOver.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelGameOver.Location = new System.Drawing.Point(52, 141);
+            this.labelGameOver.Name = "labelGameOver";
+            this.labelGameOver.Size = new System.Drawing.Size(152, 34);
+            this.labelGameOver.TabIndex = 5;
+            this.labelGameOver.Text = "Game Over";
+            this.labelGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelGameOver.Visible = false;
+            // 
+            // pictureRestart
+            // 
+            this.pictureRestart.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureRestart.Enabled = false;
+            this.pictureRestart.ImageLocation = "";
+            this.pictureRestart.InitialImage = null;
+            this.pictureRestart.Location = new System.Drawing.Point(112, 195);
+            this.pictureRestart.Name = "pictureRestart";
+            this.pictureRestart.Size = new System.Drawing.Size(32, 31);
+            this.pictureRestart.TabIndex = 6;
+            this.pictureRestart.TabStop = false;
+            this.pictureRestart.Click += new System.EventHandler(this.pictureRestart_Click);
+            // 
+            // pictureQuit
+            // 
+            this.pictureQuit.Enabled = false;
+            this.pictureQuit.Location = new System.Drawing.Point(112, 241);
+            this.pictureQuit.Name = "pictureQuit";
+            this.pictureQuit.Size = new System.Drawing.Size(32, 31);
+            this.pictureQuit.TabIndex = 7;
+            this.pictureQuit.TabStop = false;
+            this.pictureQuit.Click += new System.EventHandler(this.pictureQuit_Click);
+            // 
+            // picturePause
+            // 
+            this.picturePause.Location = new System.Drawing.Point(214, 3);
+            this.picturePause.Name = "picturePause";
+            this.picturePause.Size = new System.Drawing.Size(32, 31);
+            this.picturePause.TabIndex = 8;
+            this.picturePause.TabStop = false;
+            this.picturePause.Click += new System.EventHandler(this.picturePause_Click);
+            // 
+            // panelDisplay
+            // 
+            this.panelDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDisplay.Controls.Add(this.labelTimer);
+            this.panelDisplay.Controls.Add(this.picturePause);
+            this.panelDisplay.Controls.Add(this.labelScore);
+            this.panelDisplay.Controls.Add(this.labelMissed);
+            this.panelDisplay.Location = new System.Drawing.Point(2, 8);
+            this.panelDisplay.Name = "panelDisplay";
+            this.panelDisplay.Size = new System.Drawing.Size(251, 31);
+            this.panelDisplay.TabIndex = 0;
+            // 
+            // timerBallSpawn
+            // 
+            this.timerBallSpawn.Interval = 5000;
+            this.timerBallSpawn.Tick += new System.EventHandler(this.timerBallSpawn_Tick);
             // 
             // GDIForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(243, 342);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(255, 357);
+            this.Controls.Add(this.panelDisplay);
+            this.Controls.Add(this.pictureQuit);
+            this.Controls.Add(this.pictureRestart);
+            this.Controls.Add(this.labelGameOver);
+            this.Controls.Add(this.labelMouseCoord);
+            this.Controls.Add(this.labelBallCoord);
             this.DoubleBuffered = true;
             this.Name = "GDIForm";
             this.Text = "GDIForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GDIForm_FormClosing);
             this.Load += new System.EventHandler(this.GDIForm_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.GDIForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GDIForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GDIForm_KeyUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GDIForm_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRestart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureQuit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePause)).EndInit();
+            this.panelDisplay.ResumeLayout(false);
+            this.panelDisplay.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labelBallCoord;
+        private System.Windows.Forms.Label labelMouseCoord;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelMissed;
+        private System.Windows.Forms.Label labelGameOver;
+        private System.Windows.Forms.PictureBox pictureRestart;
+        private System.Windows.Forms.PictureBox pictureQuit;
+        private System.Windows.Forms.PictureBox picturePause;
+        private System.Windows.Forms.Panel panelDisplay;
+        private System.Windows.Forms.Timer timerBallSpawn;
     }
 }
--- GDI/Forms/GDIForm.cs
+++ GDI/Forms/GDIForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -39,7 +40,8 @@ namespace GDI
         private UInt32 timerId;
 
         private UInt32 dwUser = 0;
-
+        TimerCallback timerCallback;
+        GCHandle timerHandle;
 
 
 
@@ -47,118 +49,189 @@ namespace GDI
         {
             try
             {
-                Invoke((Action)IncLabel);
+                if (!this.IsDisposed) this.Invoke((Action)IncLabel);
             }
-            finally
+            catch
             {
-                TimeKillEvent(timerId);
-                timerId = TimeSetEvent(uDelay, uResolution, Timer_Tick, ref dwUser, TIME_ONESHOT);
+
             }
 
         }
-
 
 
         void IncLabel()
         {
-            this.Text = (int.Parse(this.Text) + 1).ToString();
+
+            if (!this.IsDisposed)
+            {
+                this.Text = (int.Parse(this.Text) + 1).ToString();
+
+            }
+
+        }
+
+        void StartMmTimer()
+        {
+            this.Text = "1";
+            uDelay = 10;
+            uResolution = 10;
+            timerCallback = new TimerCallback(Timer_Tick);
+            timerHandle = GCHandle.Alloc(timerCallback);
+
+            timerId = TimeSetEvent(uDelay, uResolution, timerCallback, ref dwUser, TIME_PERIODIC);
+        }
+        void StopMmTimer()
+        {
+            TimeKillEvent(timerId);
+            timerHandle.Free();
         }
         #endregion
-
-        static public GDIForm form = new GDIForm();
-
-        Ball ball;
-        int mouse_pos_X;
-        int mouse_pos_Y;
+        static private GDIForm form = new GDIForm();
+        static private GDIForm parent;
+        static private bool pause = false;
+        private Random random;
+        private int mouse_pos_X;
+        private int mouse_pos_Y;
+        private List<Ball> balls = new List<Ball>();
+        private RocketMoveDirections rocketMove = RocketMoveDirections.None;
+        private Rocket rocket;
+        private int score;
+        private int missed;
+        
+        
+        
 
         public GDIForm()
         {
             InitializeComponent();
-         
-
+            random = new Random();
+            parent = this;
+            score = 0;
+            missed = 0;
+            
+            rocket = new Rocket
+            {
+                X = this.Width / 2,
+                H = 15,
+                W = 50,
+                Brush = new SolidBrush(Color.Tomato)
+            };
 
 
 
         }
 
+        public void Score()
+        {
+
+            labelScore.Text = $"Score: {score}";
+            labelMissed.Text = $"Missed: {missed}";
+        }
 
 
         private void GDIForm_Paint(object sender, PaintEventArgs e)
         {
 
-            e.Graphics.DrawRectangle(ball.clearPen, ball.X, ball.Y, ball.W, ball.H);
-            //e.Graphics.FillEllipse(ballBrush, ball.X, ball.Y, ball.W, ball.H);
+            foreach (Ball ball in balls)
+            {
 
-            ball.Move();
+                e.Graphics.DrawRectangle(ball.clearPen, ball.X, ball.Y, ball.W, ball.H);
+                //e.Graphics.FillEllipse(ballBrush, ball.X, ball.Y, ball.W, ball.H);
 
-            e.Graphics.DrawRectangle(ball.ballPen, ball.X, ball.Y, ball.W, ball.H);
-            //e.Graphics.FillEllipse(ballBrush, ball.X, ball.Y, ball.W, ball.H);
+                ball.Move(panelDisplay);
 
+                e.Graphics.DrawRectangle(ball.ballPen, ball.X, ball.Y, ball.W, ball.H);
+                //e.Graphics.FillEllipse(ballBrush, ball.X, ball.Y, ball.W, ball.H);
+            }
 
+            switch (rocketMove)
+            {
+                case RocketMoveDirections.Left:
+                    e.Graphics.FillRectangle(rocket.Brush, rocket.X, this.ClientSize.Height - rocket.H, rocket.W, rocket.H);
+                    rocket.V = -10;
+                    rocket.Move();
+                    e.Graphics.FillRectangle(rocket.Brush, rocket.X, this.ClientSize.Height - rocket.H, rocket.W, rocket.H);
+                    break;
+                case RocketMoveDirections.Right:
+                    e.Graphics.FillRectangle(rocket.Brush, rocket.X, this.ClientSize.Height - rocket.H, rocket.W, rocket.H);
+                    rocket.V = 10;
+                    rocket.Move();
+                    break;
+                case RocketMoveDirections.None:
+                    break;
+
+            }
+
+            e.Graphics.FillRectangle(rocket.Brush, rocket.X, this.ClientSize.Height - rocket.H, rocket.W, rocket.H);
+
+            Ball toRemove = null;
+            foreach (Ball ball in balls)
+            {
+                if (ball.Y >= this.ClientSize.Height)
+                {
+                    toRemove = ball;
+                }
+                else if (ball.Y + ball.H >= this.ClientSize.Height - rocket.H && ball.Y + ball.H <= this.ClientSize.Height)
+                {
+                    if (ball.X + ball.W / 2 >= rocket.X && ball.X + ball.W / 2 <= rocket.X + rocket.W)
+                    {
+                        score += 1;
+                        ball.Vy = -ball.Vy;
+                        ball.Y = this.ClientSize.Height - rocket.H - ball.H;
+                    }
+
+                }
+
+            }
+            if (toRemove != null)
+            {
+                missed += 1;
+                balls.Remove(toRemove);
+            }
+            
         }
 
         private void GDIForm_Load(object sender, EventArgs e)
         {
-            /* this.Text = "1";
-             uDelay = 10;
-             uResolution = 3;
-
-             timerId = TimeSetEvent(uDelay, uResolution, Timer_Tick ,ref dwUser, TIME_ONESHOT);*/
-            ball = new Ball
-            {
-                X = form.ClientSize.Width / 2,
-                Y = form.ClientSize.Height / 2,
-                W = 20,
-                H = 20,
-                Vx = -2,
-                Vy = -2,
-                clearBrush = new SolidBrush(form.BackColor),
-                ballBrush = new SolidBrush(Color.Tomato),
-                clearPen = new Pen(form.BackColor),
-                ballPen = new Pen(Color.Tomato, 3)
-            };
-
-
-            
-            timer1.Start();
-
-
+            GameLoad();
         }
 
         private void GDIForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            //TimeKillEvent(timerId);
-            timer1.Stop();
+            //StopMmTimer();
+            timer.Stop();
         }
 
-
-
-        private void timer1_Tick(object sender, EventArgs e)
+        private void GDIForm_MouseMove(object sender, MouseEventArgs e)
         {
-
-            Invalidate();
-
+            mouse_pos_X = Convert.ToInt32(Control.MousePosition.X);
+            mouse_pos_Y = Convert.ToInt32(Control.MousePosition.Y);
+            labelMouseCoord.Text = $"{mouse_pos_X}.{mouse_pos_Y}";
         }
+
 
         class Ball
         {
+
 
             public SolidBrush clearBrush { get; set; }
             public SolidBrush ballBrush { get; set; }
             public Pen clearPen { get; set; }
             public Pen ballPen { get; set; }
-            public Point Position { get; set; }
-           
+
+            public Label labelMouseCoord { get; set; }
+
+            public int PositionX { get; set; }
+            public int PositionY { get; set; }
+
             public int X { get; set; }
             public int Y { get; set; }
             public int W { get; set; }
             public int H { get; set; }
             public int Vx { get; set; }
             public int Vy { get; set; }
-            public Cursor Cursor { get; private set; }
-            public int Location { get; private set; }
 
-            public void Move()
+            public void Move(Panel panel)
             {
 
                 X += Vx;
@@ -167,11 +240,15 @@ namespace GDI
                 {
                     X = 0;
                     Vx = -Vx;
+
+
                 }
-                if (Y < 0)
+                //Проверям Y с учётом размера панели по высоте окна
+                if (Y < panel.Height)
                 {
-                    Y = 0;
+                    Y = panel.Height + 1;
                     Vy = -Vy;
+
                 }
 
                 //Проверям X с учётом размера шара по ширине окна
@@ -179,22 +256,204 @@ namespace GDI
                 {
                     X = form.ClientSize.Width - W;
                     Vx = -Vx;
+
                 }
                 //Проверям Y с учётом размера шара по высоте окна
-                if (Y + H > form.ClientSize.Height)
-                {
-                    Y = form.ClientSize.Height - H;
-                    Vy = -Vy;
-                }
+                /* if (Y + H > form.ClientSize.Height)
+                 {
+
+                     Y = form.ClientSize.Height - H;
+                     Vy = -Vy;
+                 }*/
+
+                PositionX = X;
+                PositionY = Y;
 
 
             }
 
-         
-
-       
         }
 
+        class Rocket
+        {
+            public int X { get; set; }
+            public int W { get; set; }
+            public int H { get; set; }
+            public int V { get; set; }
+
+            public Brush Brush { get; set; }
+
+            public void Move()
+            {
+                X += V;
+                if (X < 0)
+                {
+                    X = 0;
+                    V = -V;
+                }
+                if (X + W >= parent?.ClientSize.Width)
+                {
+                    X = parent.ClientSize.Width - W;
+                    V = -V;
+                }
+
+            }
+        }
+
+        private void GDIForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                rocketMove = RocketMoveDirections.Left;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                rocketMove = RocketMoveDirections.Right;
+            }
+        }
+
+        private void GDIForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && rocketMove == RocketMoveDirections.Left)
+            {
+                rocketMove = RocketMoveDirections.None;
+            }
+            if (e.KeyCode == Keys.Right && rocketMove == RocketMoveDirections.Right)
+            {
+                rocketMove = RocketMoveDirections.None;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (balls.Count > 0)
+            {
+                labelBallCoord.Text = $"{balls[0].PositionX}.{balls[0].PositionY}";
+                
+            }
+            else
+            {
+                timer.Stop();
+                timerBallSpawn.Stop();
+                labelGameOver.Visible = true;
+                pictureQuit.Enabled = true;
+                pictureQuit.Visible = true;
+                pictureRestart.Enabled = true;
+                pictureRestart.Visible = true;
+
+            }
+
+            picturePause.Image = Image.FromFile("icons-pause.bmp");
+            Score();
+            Invalidate();
+        }
+
+        enum RocketMoveDirections
+        {
+            None,
+            Left,
+            Right
+        }
+
+
+
+        
+
+        //Метод спавна шарика
+        private void SpawnBall()
+        {
+            for (int ball_index = 0; ball_index < 1; ball_index++)
+            {
+
+                balls.Add(new Ball
+                {
+
+                    X = form.ClientSize.Width / 6 + random.Next(form.ClientSize.Width),
+                    W = 20,
+                    H = 20,
+
+                    Vx = random.Next(1, 10),
+                    Vy = random.Next(1, 10),
+
+                    clearBrush = new SolidBrush(form.BackColor),
+                    ballBrush = new SolidBrush(Color.Tomato),
+                    clearPen = new Pen(form.BackColor),
+                    ballPen = new Pen(Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)))
+                });
+            }
+
+
+        }
+
+        //Переопределённый метод спавна принимающий кол-во шариков
+        private void SpawnBall(int amount)
+        {
+            for (int ball_index = 0; ball_index < amount; ball_index++)
+            {
+                SpawnBall();
+            }
+        }
+
+        //Метод загрузки игры
+        private void GameLoad()
+        {
+            timerBallSpawn.Start();
+            labelGameOver.Visible = false;
+            pictureRestart.Visible = false;
+            pictureRestart.Enabled = false;
+            pictureQuit.Visible = false;
+            pictureQuit.Enabled = false;
+
+            score = 0;
+            missed = 0;
+
+            pictureRestart.Image = Image.FromFile("icons-restart.bmp");
+            pictureQuit.Image = Image.FromFile("icons-quit.bmp");
+
+            SpawnBall(1);
+            timer.Start();
+        }
+
+        //Событие таймера на спавн шариков
+        private void timerBallSpawn_Tick(object sender, EventArgs e)
+        {
+            SpawnBall();
+        }
+
+
+
+        private void picturePause_Click(object sender, EventArgs e)
+        {
+
+
+            if (pause == false)
+            {
+                pause = true;
+                timer.Stop();
+                picturePause.Image = Image.FromFile("icons-resume.bmp");
+
+
+            }
+            else
+            {
+                pause = false;
+                timer.Start();
+                picturePause.Image = Image.FromFile("icons-pause.bmp");
+            }
+
+
+
+        }
+
+        private void pictureRestart_Click(object sender, EventArgs e)
+        {
+            GameLoad();
+        }
+
+        private void pictureQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
 
     }
 
